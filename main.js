13ec24b24cb6dfbc3c46b8138da3e3132853124f
Pablo Torres <a01630637@itesm.mx>
2022-04-01 23:25:08 -0600

    Navigation indicator sizes (repair)

--- main.js
+++ main.js
@@ -1,17 +1,28 @@
 //Execute script after DOM is loaded
 document.addEventListener('DOMContentLoaded', function(event) {
 
+    // Default theme mode
+    (function() {
+        if (localStorage.getItem('theme') === 'light-mode') {
+            setTheme('light-mode');
+        } else {
+            setTheme('dark-mode');
+        }
+    })();
     /* // Navigation tabs */
     const indicator = document.getElementById('indicator');
     const navigation = document.getElementById('navigation');
     const sections = document.querySelectorAll('.sections');
     const contentTab = document.querySelectorAll('[data-content]');
 
+    /*pages*/
+    /* const pages = document.querySelectorAll('.pages'); */
+
     /* Selector size */
     let sizeIndicator = navigation.querySelector('li').offsetWidth;
     let activeSection;
-    indicator.style.width = sizeIndicator + 39 + 'px';
-    console.log(sizeIndicator);
+    indicator.style.width = 20 + sizeIndicator + 'px';
+
     /* Navigation */
     const targets = navigation.querySelectorAll('[data-target]');
     targets.forEach(target => {
@@ -19,40 +30,51 @@ document.addEventListener('DOMContentLoaded', function(event) {
 
             /* Removing active class to all elements */
             contentTab.forEach(c => {
-                c.classList.remove('active')
-                indicator.style.transform = `translateX(${128*0}px)`;
+                c.classList.remove('active');
             })
             const t = document.querySelector(target.dataset.target);
             t.classList.add('active');
-        })
-    })
+            /* console.log(t) */
+        });
+    });
+    // Change indicator size on display size change
     const sizeChange = () => {
         sizeIndicator = navigation.querySelector('li').offsetWidth;
-        indicator.style.width = `${sizeIndicator}px`;
-    }
-    const observer = new IntersectionObserver((entradas, observer) => {
+        indicator.style.width = `${40+sizeIndicator}px`;
+    };
+    window.addEventListener('resize', sizeChange);
+    const observer = new IntersectionObserver((entradas) => {
         entradas.forEach(entrada => {
             if (entrada.isIntersecting) {
                 /* ------ Transform into array ------- */
                 activeSection = [...sections].indexOf(entrada.target);
-                /* indicator.style.transform = `translateX(${sizeIndicator*activeSection}px)`; */
-                indicator.style.transform = `translateX(${128*activeSection}px)`;
+                indicator.style.transform = `translateX(${(30+sizeIndicator)*activeSection}px)`;
+                if (sizeIndicator > 77) {
+                    console.log(sizeIndicator);
+                    indicator.style.transform = `translateX(${(75+sizeIndicator)*activeSection}px)`;
+                }
+                /* indicator.style.transform = `translateX(${129*activeSection}px)`; */
             }
-
         });
     });
     sections.forEach(section => observer.observe(section));
 
-    window.addEventListener('resize', sizeChange);
-})
+    /* pagination */
+    const previous = document.querySelector('previous--button');
+    const next = document.querySelector('next--button');
+    const pages = document.querySelectorAll('.pages');
+    previous.addEventListener('click', function() {
+        previous();
+    });
 
+});
 
-
-/* sections.forEach(section => {
-    section.
-}) */
-
-
+function previous() {
+    pages.forEach(page => {
+        console.log(page);
+        page.classList.add('.pageActive');
+    });
+}
 
 // function to set dark/light modes
 function setTheme(themeName) {
@@ -68,12 +90,3 @@ function toggleThemeMode() {
         setTheme('light-mode');
     }
 }
-
-// Immediately invoked function to set the theme on initial load
-(function() {
-    if (localStorage.getItem('theme') === 'light-mode') {
-        setTheme('light-mode');
-    } else {
-        setTheme('dark-mode');
-    }
-})();
